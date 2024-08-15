25e3545451e5dbc5c1a6f09c21b729327621c797
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-15 17:18:15 +0530

    RevNum Project Setup

---
+++ src/RevNumClient/src/MyRevNumServer.java
@@ -0,0 +1,10 @@
+package RevNumClient.src;
+
+public class MyRevNumServer {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/RevNumServer/src/MyRevNumClient.java
@@ -0,0 +1,9 @@
+package RevNumServer.src;
+
+public class MyRevNumClient {
+
+	public static void main(String[] args) {
+
+	}
+
+}
