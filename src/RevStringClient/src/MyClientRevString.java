4595455fcf9010be3a21ec28f0e2e94c86c9afb6
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-15 17:33:02 +0530

    RevString Project Setup

---
+++ src/RevStringClient/src/MyClientRevString.java
@@ -0,0 +1,9 @@
+package RevStringClient.src;
+
+public class MyClientRevString {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/RevStringServer/src/MyServerRevString.java
@@ -0,0 +1,9 @@
+package RevStringServer.src;
+
+public class MyServerRevString {
+
+	public static void main(String[] args) {
+
+	}
+
+}
