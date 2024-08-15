79aeebbd0ecc3058a6a92b2e436ef4b60ce82f81
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-15 12:27:22 +0530

    Factorial Server-Client Setup

---
+++ src/FactClient/src/MyClientFact.java
@@ -0,0 +1,10 @@
+package FactClient.src;
+
+public class MyClientFact {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ src/FactServer/src/MyServerFact.java
@@ -0,0 +1,10 @@
+package FactServer.src;
+
+public class MyServerFact {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
