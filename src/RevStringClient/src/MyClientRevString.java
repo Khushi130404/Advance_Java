ae83c573ae9ffef8a6c2dc6c741c3a2978fb4431
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-15 17:37:06 +0530

    Client-Side RevString

--- src/RevStringClient/src/MyClientRevString.java
+++ src/RevStringClient/src/MyClientRevString.java
@@ -1,9 +1,34 @@
 package RevStringClient.src;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class MyClientRevString {
 
 	public static void main(String[] args) {
-
+		try {
+			Socket socket = new Socket("localhost",1304);
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.print("Enter a String : ");
+			String s = scan.next();
+			
+			dos.writeUTF(s);
+			
+			String st = dis.readUTF();
+			System.out.println("Reverse String : "+st);
+			
+			dos.close();
+			dis.close();
+			socket.close();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
-
 }
