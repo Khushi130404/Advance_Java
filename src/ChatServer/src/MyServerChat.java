fb060876099a61110d35756595ca91258b68a7b1
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-16 17:32:59 +0530

    Server Side

--- src/ChatServer/src/MyServerChat.java
+++ src/ChatServer/src/MyServerChat.java
@@ -1,9 +1,43 @@
 package ChatServer.src;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MyServerChat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ServerSocket server = new ServerSocket(1304);
+			System.out.println("Server waiting to Connect");
+			Socket client = server.accept();
+			System.out.println("Welcome Client");
+			
+			DataInputStream dis = new DataInputStream(client.getInputStream());
+			DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+			
+			String s = "";
+			
+			do 
+			{
+				System.out.print("Enter a String...");
+				s = dis.readUTF();
+				System.out.print("Client : "+s);
+			}while(s.equalsIgnoreCase("Q"));
+			
+			
+			dos.close();
+			dis.close();
+			client.close();
+			server.close();
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
 
 	}
 
