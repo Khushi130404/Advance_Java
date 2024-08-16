62cb340ac943dff1eea852d9928757fe1e8b8a2d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-16 18:24:40 +0530

    Client-Server End

--- src/ChatClient/src/MyClientChat.java
+++ src/ChatClient/src/MyClientChat.java
@@ -9,7 +9,9 @@ public class MyClientChat {
 
 	public static void main(String[] args) {
 		try {
-			Socket socket = new Socket("localhost",1300);
+//			Socket socket = new Socket("192.168.206.243",2233);
+			Socket socket = new Socket("192.168.29.102",2001);
+//			Socket socket = new Socket("localhost",1300);
 			DataInputStream dis = new DataInputStream(socket.getInputStream());
 			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 			Scanner scan = new Scanner(System.in);
@@ -19,12 +21,14 @@ public class MyClientChat {
 			while(!s.equalsIgnoreCase("Quit"))
 			{
 				s = dis.readUTF();
+				if(s.equalsIgnoreCase("Quit")) break;
 				System.out.println("Server : "+s);
 				System.out.print("Enter a String...");
 				s = scan.next();
 				dos.writeUTF(s);
+				if(s.equalsIgnoreCase("Quit")) break;
 			}
-			
+			System.out.print("Program End");
 			dos.close();
 			dis.close();
 			socket.close();
