21368aa4ce92e1e3ae69b95c8479aad872d4ac98
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-16 17:29:07 +0530

    ChatBox Project Setup

---
+++ src/ChatClient/src/MyClientChat.java
@@ -0,0 +1,10 @@
+package ChatClient.src;
+
+public class MyClientChat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ChatServer/src/MyServerChat.java
@@ -0,0 +1,10 @@
+package ChatServer.src;
+
+public class MyServerChat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
