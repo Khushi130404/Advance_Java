838c5d1b0d202078a48b20d261b8b9ea984f7736
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-20 17:49:59 +0530

    Receiver Side UpLow

--- src/UDPReceiverUpLow/src/MyReceiver.java
+++ src/UDPReceiverUpLow/src/MyReceiver.java
@@ -1,10 +1,31 @@
 package UDPReceiverUpLow.src;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class MyReceiver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			DatagramSocket receiver = new DatagramSocket(1340);
+			byte[] b = new byte[1024];
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Message : ");
+			String msg = scan.next();
+			b = msg.getBytes();
+			InetAddress ip = InetAddress.getByName("localhost");
+			DatagramPacket packet = new DatagramPacket(b,b.length,ip,1304);
+			receiver.send(packet);
+			receiver.receive(packet);
+			msg = new String(b);
+			System.out.println("Upper : "+msg);
+			receiver.close();
+		} catch (Exception e) {
+			System.out.println("Error : "+e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
