b0fdb83da1b5eece0e9cfe5abd210478f87b4a9b
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-20 18:12:11 +0530

    Receiver Side Reverse

--- src/UDPReceiverRev/src/MyReceiver.java
+++ src/UDPReceiverRev/src/MyReceiver.java
@@ -1,11 +1,29 @@
 package UDPReceiverRev.src;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class MyReceiver {
 
 	public static void main(String[] args) {
 		try
 		{
-			
+			DatagramSocket receiver = new DatagramSocket(4321);
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Message : ");
+			String msg = scan.nextLine();
+			byte b[] = new byte[1024];
+			b = msg.getBytes();
+			InetAddress ip = InetAddress.getByName("localhost");
+			DatagramPacket pack = new DatagramPacket(b, b.length, ip, 1234);
+			receiver.send(pack);
+			receiver.receive(pack);
+			b = pack.getData();
+			msg = new String(b);
+			System.out.println("Reverse : "+msg);
+			receiver.close();
 		}
 		catch (Exception e) {
 			System.out.println("Receiv Err : "+e.getMessage());
