7c5cdfed36530a52b144e47e651cbc5ab156730a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-20 17:46:06 +0530

    Sender Side UpLow

--- src/UDPSenderUpLow/src/MySender.java
+++ src/UDPSenderUpLow/src/MySender.java
@@ -1,10 +1,30 @@
 package UDPSenderUpLow.src;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
 public class MySender {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			DatagramSocket sender = new DatagramSocket(1304);
+			String msg = "";
+			byte[] b = new byte[1024];
+			DatagramPacket packet = new DatagramPacket(b,b.length);
+			sender.receive(packet);
+			msg = new String(b);
+			msg = msg.toUpperCase();
+			b = msg.getBytes();
+			InetAddress ip = InetAddress.getByName("localhost");
+			packet = new DatagramPacket(b, b.length,ip,1340);
+			sender.send(packet);
+			sender.close();
+		} catch (Exception e) {
+			System.out.println("Error : "+e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
