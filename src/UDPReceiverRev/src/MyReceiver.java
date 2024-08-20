17bb8ec6446d9103d675b5b567629abdefb5beb7
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-20 18:08:31 +0530

    Sender Side Reverse

--- src/UDPReceiverRev/src/MyReceiver.java
+++ src/UDPReceiverRev/src/MyReceiver.java
@@ -3,8 +3,14 @@ package UDPReceiverRev.src;
 public class MyReceiver {
 
 	public static void main(String[] args) {
-		
-
+		try
+		{
+			
+		}
+		catch (Exception e) {
+			System.out.println("Receiv Err : "+e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/UDPSenderRev/src/MySender.java
+++ src/UDPSenderRev/src/MySender.java
@@ -1,9 +1,35 @@
 package UDPSenderRev.src;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class MySender {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try
+		{
+			DatagramSocket sender = new DatagramSocket(1234);
+			byte b[] =  new byte[1024];
+			String msg = "";
+			DatagramPacket pack = new DatagramPacket(b, b.length);
+			sender.receive(pack);
+			b = pack.getData();
+			msg = new String(b);
+			StringBuffer sb = new StringBuffer(msg);
+			sb.reverse();
+			msg = sb.toString();
+			b = msg.getBytes();
+			InetAddress ip = InetAddress.getByName("localhost");
+			pack = new DatagramPacket(b, b.length, ip, 4321);
+			sender.send(pack);
+			sender.close();
+		}
+		catch (Exception e) {
+			System.out.println("Sender Err : "+e.getMessage());
+			e.printStackTrace();
+		}
 
 	}
 
